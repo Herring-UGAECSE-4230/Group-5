e891208711380204ce3a24a90c6d2663be9f5a11
Christian Jensen <104859200+cjj03019@users.noreply.github.com>
2024-01-23 22:58:16 -0500

    Added Deliverables to HelloWorld.s

--- Christian_Assembly/Hello_World/HelloWorld.s
+++ Christian_Assembly/Hello_World/HelloWorld.s
@@ -2,6 +2,11 @@
 @R0-R2- parameters to linux function services
 @R7 - linux function number
 
+@Deliverables:
+@1. The code used to display "Hello World from Group XXX" in the terminal with comments.
+@2. A screenshot of the terminal output.
+@3. Describe what the makefile is and what it is doing for this program.
+
 @Provide program starting point
 .global _start
 
