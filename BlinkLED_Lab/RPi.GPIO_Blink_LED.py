ecabc5585b48c58b31e0b938016ea851ab4a1be5
Christianj916 <chrisjjensen16@gmail.com>
2024-01-25 13:29:18 -0500

    Changed RPi.GPIO_Blink_LED.py to where it should output a high then low signal after one second

--- BlinkLED_Lab/RPi.GPIO_Blink_LED.py
+++ BlinkLED_Lab/RPi.GPIO_Blink_LED.py
@@ -2,3 +2,9 @@ import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(3,GPIO.OUT, initial=GPIO.LOW)
+
+while True:
+    GPIO.output(3, GPIO.HIGH)
+    sleep(1)
+    GPIO.output(3, GPIO.LOW)
