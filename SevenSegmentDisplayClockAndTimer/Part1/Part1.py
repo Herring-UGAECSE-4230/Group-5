458795ef115534c77e387993c4c3114ee59c3c15
Christian Jensen <Cjj03019@uga.edu>
2024-02-06 21:23:38 -0500

    Fixed errors on Part1.py

--- SevenSegmentDisplayClockAndTimer/Part1/Part1.py
+++ SevenSegmentDisplayClockAndTimer/Part1/Part1.py
@@ -1,23 +1,39 @@
 import RPi.GPIO as GPIO
 from time import sleep
 # The Segments are:     A   B   C   D   E   F   G
-gpioSevenSegmentPins = [12, 16, 23, 18, 4, 25, 24]
+gpioSevenSegmentPins = [27, 17, 18, 23, 24, 22, 5]
 # Keypad Map:           x1  x2  x3  x4  y1  y2  y3  y4
-gpioKeypadPins =       [26, 19, 13, 16, 5,  22, 26, 17] 
+gpioKeypadPins =       [21, 20, 16, 12 , 6 ,13, 19, 26]
 #In order from 0 to 9
-sevenSegment0 = [1,1,1,1,1,1,0]
-sevenSegment1 = [0,1,1,0,0,0,0]
-sevenSegment2 = [1,1,0,1,1,0,1]
-sevenSegment3 = [1,1,1,1,0,0,1]
-sevenSegment4 = [0,1,1,0,0,1,1]
-sevenSegment5 = [1,0,1,1,0,1,1]
-sevenSegment6 = [0,0,1,1,1,1,1]
-sevenSegment7 = [1,1,1,0,0,0,0]
-sevenSegment8 = [1,1,1,1,1,1,1]
-sevenSegment9 = [1,1,1,0,0,1,1]
+sevenSegment0 = (1,1,1,1,1,1,0)
+sevenSegment1 = (0,1,1,0,0,0,0)
+sevenSegment2 = (1,1,0,1,1,0,1)
+sevenSegment3 = (1,1,1,1,0,0,1)
+sevenSegment4 = (0,1,1,0,0,1,1)
+sevenSegment5 = (1,0,1,1,0,1,1)
+sevenSegment6 = (0,0,1,1,1,1,1)
+sevenSegment7 = (1,1,1,0,0,0,0)
+sevenSegment8 = (1,1,1,1,1,1,1)
+sevenSegment9 = (1,1,1,0,0,1,1)
 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+# Clock pin
+GPIO.setup(4, GPIO.OUT)
+clk = GPIO.PWM(4, 100)
 GPIO.setup(gpioSevenSegmentPins, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(gpioKeypadPins, GPIO.IN, initial = GPIO.LOW)
+# Keypad Pins Setup
+GPIO.setup(gpioKeypadPins[0:4], GPIO.OUT, initial = GPIO.LOW)
+
+
+GPIO.setup(gpioKeypadPins[4], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.setup(gpioKeypadPins[5], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.setup(gpioKeypadPins[6], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.setup(gpioKeypadPins[7], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 
 keypadMap = [['1', '2', '3', 'A'],
              ['4', '5', '6', 'B'],
@@ -28,17 +44,21 @@ keypadMap = [['1', '2', '3', 'A'],
 curVal = '0'
 
 def readKeypad(rowNum, rowChar):
-
+    global curVal
     GPIO.output(rowNum, GPIO.HIGH)
 
     if (GPIO.input(gpioKeypadPins[4]) == GPIO.HIGH):
         curVal = rowChar[0]
+        print(curVal)
     elif (GPIO.input(gpioKeypadPins[5]) == GPIO.HIGH):
         curVal = rowChar[1]
+        print(curVal)
     elif (GPIO.input(gpioKeypadPins[6]) == GPIO.HIGH):
         curVal = rowChar[2]
+        print(curVal)
     elif (GPIO.input(gpioKeypadPins[7]) == GPIO.HIGH):
         curVal = rowChar[3]
+        print(curVal)
     GPIO.output(rowNum,GPIO.LOW)
 
     return curVal
@@ -65,18 +85,18 @@ def sendToSSD(curVal):
     if (curVal == '9'):
         GPIO.output(gpioSevenSegmentPins, sevenSegment9)
 
-    
+clk.start(50);
 while True:
-    sleep(0.5)
-    if gpioKeypadPins[0] == GPIO.HIGH:
+    #sleep(0.5)[21, 20, 16, 12 , 6 ,13, 19, 26][21, 20, 16, 12 , 6 ,13, 19, 26]
         readKeypad(gpioKeypadPins[0], keypadMap[0])
         sendToSSD(curVal)
-    if gpioKeypadPins[1] == GPIO.HIGH:
+        #print("Read Input!");
         readKeypad(gpioKeypadPins[1], keypadMap[1])
         sendToSSD(curVal)
-    if gpioKeypadPins[2] == GPIO.HIGH:
+        #print("Read Input!");
         readKeypad(gpioKeypadPins[2], keypadMap[2])
         sendToSSD(curVal)
-    if gpioKeypadPins[3] == GPIO.HIGH:
+        #print("Read Input!");
         readKeypad(gpioKeypadPins[3], keypadMap[3])
         sendToSSD(curVal)
+        #print("Read Input!");
