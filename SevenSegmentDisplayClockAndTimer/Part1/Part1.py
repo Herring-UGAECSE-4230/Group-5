4d5d55a09b65c996cf19a57db9bb40cc01029b04
Christian Jensen <cjj03019@uga.edu>
2024-02-15 14:04:24 -0500

    Added DP support for SSD

--- SevenSegmentDisplayClockAndTimer/Part1/Part1.py
+++ SevenSegmentDisplayClockAndTimer/Part1/Part1.py
@@ -1,26 +1,27 @@
 import RPi.GPIO as GPIO
 from time import sleep
-# The Segments are:     A   B   C   D   E   F   G
-gpioSevenSegmentPins = [27, 17, 18, 23, 24, 22, 5]
+# The Segments are:     A   B   C   D   E   F   G  DP
+gpioSevenSegmentPins = [27, 17, 18, 23, 24, 22, 5, 7]
 # Keypad Map:           x1  x2  x3  x4  y1  y2  y3  y4
 gpioKeypadPins =       [21, 20, 16, 12 , 6 ,13, 19, 26]
 
 #Possible Seven Segment Configurations
-sevenSegment0 = (1,1,1,1,1,1,0)
-sevenSegment1 = (0,1,1,0,0,0,0)
-sevenSegment2 = (1,1,0,1,1,0,1)
-sevenSegment3 = (1,1,1,1,0,0,1)
-sevenSegment4 = (0,1,1,0,0,1,1)
-sevenSegment5 = (1,0,1,1,0,1,1)
-sevenSegment6 = (0,0,1,1,1,1,1)
-sevenSegment7 = (1,1,1,0,0,0,0)
-sevenSegment8 = (1,1,1,1,1,1,1)
-sevenSegment9 = (1,1,1,0,0,1,1)
-sevenSegmentA = (1,1,1,0,1,1,1)
-sevenSegmentB = (0,0,1,1,1,1,1)
-sevenSegmentC = (1,0,0,1,1,1,0)
-sevenSegmentD = (0,1,1,1,1,0,1)
-sevenSegmentOff = (0,0,0,0,0,0,0)
+sevenSegment0 = (1,1,1,1,1,1,0,0)
+sevenSegment1 = (0,1,1,0,0,0,0,0)
+sevenSegment2 = (1,1,0,1,1,0,1,0)
+sevenSegment3 = (1,1,1,1,0,0,1,0)
+sevenSegment4 = (0,1,1,0,0,1,1,0)
+sevenSegment5 = (1,0,1,1,0,1,1,0)
+sevenSegment6 = (0,0,1,1,1,1,1,0)
+sevenSegment7 = (1,1,1,0,0,0,0,0)
+sevenSegment8 = (1,1,1,1,1,1,1,0)
+sevenSegment9 = (1,1,1,0,0,1,1,0)
+sevenSegmentA = (1,1,1,0,1,1,1,0)
+sevenSegmentB = (0,0,1,1,1,1,1,0)
+sevenSegmentC = (1,0,0,1,1,1,0,0)
+sevenSegmentD = (0,1,1,1,1,0,1,0)
+sevenSegmentDot = (0,0,0,0,0,0,0,1)
+sevenSegmentOff = (0,0,0,0,0,0,0,0)
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -101,6 +102,8 @@ def sendToSSD(curVal):
             GPIO.output(gpioSevenSegmentPins, sevenSegmentC)
         if (curVal == 'D'):
             GPIO.output(gpioSevenSegmentPins, sevenSegmentD)
+        if (curVal == '*'):
+            GPIO.output(gpioSevenSegmentPins, sevenSegmentDot)
         if (curVal == '#'):
             GPIO.output(gpioSevenSegmentPins, sevenSegmentOff)
             ssdOn = False
