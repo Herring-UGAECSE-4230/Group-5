049fcd28db089a294efd5b986ba1661ad629d32f
Christian Jensen <cjj03019@uga.edu>
2024-02-15 23:43:41 -0500

    Fixed compilation error with 0_AddressMap and added classinclude.s

--- Christian_Assembly/EquReqData/0_AddressMap.s
+++ Christian_Assembly/EquReqData/0_AddressMap.s
@@ -4,7 +4,7 @@
 @ use gdb disas to display the dissasembly and gdb x/12xw _start to display 10 words of memory.  
 @ Deliverable 2: Make a table with the addresses representing 4 byte rows and provide the value at each byte.
 @ Example:	00004000	87	65	43	21
-@ In the example: byte 4000 contains 21, byte 4001 contains 43, byte 4002 contains 65, byte 4003 contains 87 the full word is: 0x87654321
+@ In the examplse: byte 4000 contains 21, byte 4001 contains 43, byte 4002 contains 65, byte 4003 contains 87 the full word is: 0x87654321
 @ The next address row would be 00004004.  Have your table count down by addresses so the top row is lowest address.
 
 
@@ -17,7 +17,7 @@ _start:
 	@ terminate the program
 	mov	r7, #1			
 	svc	0
- 
+	
 our_fixed_data:    					
 	.byte	0x55, 0x33, 1, 2, 3, 4, 5, 6		
 	.word	0x23222120, 0x30		
