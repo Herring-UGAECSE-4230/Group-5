523f984f31351af9316dbb55f6f8a44ef734c66f
Christian Jensen <cjj03019@uga.edu>
2024-02-18 21:48:03 -0500

    Updated 2_req_p2_2.s

--- Christian_Assembly/EquReqData/2_req_p2_2.s
+++ Christian_Assembly/EquReqData/2_req_p2_2.s
@@ -1,6 +1,8 @@
 @ Deliverable: Describe what .req is used for.  How is this different than .equ?
 @ Deliverable: Add the classinclude.s include file with changes to last two lines.
 
+.include "classinclude.s"
+	
 val1	.req r1
 val2	.req r2
 sum	.req r0
@@ -9,5 +11,5 @@ sum	.req r0
 _start:	mov	val1, #0x25
 	mov	val2, #0x34
 	add	sum, val1, val2
-	mov	r7, #1
-	svc	0
+	mov	r7, #sys_exit
+	svc	sys_restart_syscall
