1ef51e6ec135bd76fc9bc5c1956eb27f155b5087
Christian Jensen <104859200+cjj03019@users.noreply.github.com>
2024-03-13 19:02:10 -0400

    Testing to see if code is able to detect rotary rotation and print counter

--- MOSFETMotorEncoder/DigitalEncoderInput/Part1.py
+++ MOSFETMotorEncoder/DigitalEncoderInput/Part1.py
@@ -1,1 +1,29 @@
-Test
+import RPi.GPIO as GPIO
+from time import sleep
+
+# Defines the pins of the encoder
+clk = 1
+dt = 2
+sw = 3
+
+# Sets up the pins for GPIO use
+GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Holds value of previous clk state
+lastClkState = GPIO.input(clk)
+
+# Holds value that the encoder is currently on.
+counter = 0
+while True:
+    clkState=GPIO.input(clk)
+    dtState=GPIO.input(dt)
+    if clkState!=lastClkState:
+        if dtState!=clkState:
+            counter+=1
+        else:
+            counter-=1
+    lastClkState=clkState
+    print(counter)
+
