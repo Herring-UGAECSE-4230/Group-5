4294d3929aef5403c611ba6190bea1ebb87fe26e
Christian Jensen <cjj03019@uga.edu>
2024-03-28 12:55:34 -0400

    Final motor driver

--- MOSFETMotorEncoder/NMOSMotorDriver/MotorDriver.py
+++ MOSFETMotorEncoder/NMOSMotorDriver/MotorDriver.py
@@ -1,7 +1,9 @@
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-pwmPin = 5
+GPIO.setwarnings(False)
+
+pwmPin = 19
 
 #Sets up PWM pin
 GPIO.setup(pwmPin, GPIO.OUT, initial = GPIO.LOW)
@@ -9,12 +11,12 @@ GPIO.setup(pwmPin, GPIO.OUT, initial = GPIO.LOW)
 #Sets up PWM function (pwmPin, frequency)
 pwm = GPIO.PWM(pwmPin, 10)
 #Changes pwm frequeny (frequency)
-pwm = pwm.ChangeFrequency(100)
+pwm.ChangeFrequency(10)
 #Starts pwm (duty cycle)
 pwm.start(50)
 
 i = 0
-while i < 1000():
+while i < 100000000000:
     i = i + 1
     
 #Stops pwm
